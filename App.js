347fd01e500e9b0ced0a4475c7d86dde0505eedf
Dat Manh Vo <vomanhdat10998@gmail.com>
2021-01-26 09:39:34 +0700

    init

--- App.js
+++ App.js
@@ -1,109 +1,14 @@
 import React from "react";
-import {
-  StyleSheet,
-  View,
-  Text,
-  TouchableOpacity,
-  FlatList,
-  Modal,
-} from "react-native";
-
-import { Entypo } from "@expo/vector-icons";
-import colors from "./Colors";
-import tempData from "./tempData";
-import TodoList from "./components/TodoList";
-import AddListModal from "./components/AddListModal";
+import { StyleSheet, View, Text } from "react-native";
 
 export default class App extends React.Component {
-  state = {
-    addTodoVisible: false,
-  };
-
-  toggleAddTodoModal() {
-    this.setState({
-      addTodoVisible: !this.state.addTodoVisible,
-    });
-  }
-
-  renderList(item) {
-    return <TodoList list={item} />;
-  }
-
   render() {
     return (
-      <View style={styles.container}>
-        <Modal
-          animationType="slide"
-          visible={this.state.addTodoVisible}
-          onRequestClose={() => this.toggleAddTodoModal()}
-        >
-          <AddListModal closeModal={() => this.toggleAddTodoModal()} />
-        </Modal>
-        <View style={{ flexDirection: "row" }}>
-          <View style={styles.divider} />
-          <Text style={styles.title}>
-            Todo
-            <Text style={{ fontWeight: "300", color: colors.blue }}>Lists</Text>
-          </Text>
-          <View style={styles.divider} />
-        </View>
-
-        <View style={{ marginVertical: 48 }}>
-          <TouchableOpacity
-            style={styles.addList}
-            onPress={() => this.toggleAddTodoModal()}
-          >
-            <Entypo name="plus" size={24} color={colors.lightBlue} />
-          </TouchableOpacity>
-
-          <Text style={styles.add}>Add List</Text>
-        </View>
-
-        <View style={{ height: 275, paddingLeft: 32 }}>
-          <FlatList
-            data={tempData}
-            keyExtractor={(item) => item.name}
-            horizontal={true}
-            showsHorizontalScrollIndicator={false}
-            renderItem={({ item }) => this.renderList(item)}
-          />
-        </View>
+      <View>
+        <Text>This is app react native</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  divider: {
-    backgroundColor: colors.lightBlue,
-    height: 1,
-    flex: 1,
-    alignSelf: "center",
-  },
-  title: {
-    fontSize: 38,
-    fontWeight: "800",
-    color: colors.black,
-    paddingHorizontal: 64,
-  },
-  addList: {
-    borderWidth: 2,
-    borderColor: colors.lightBlue,
-    borderRadius: 4,
-    padding: 16,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  add: {
-    color: colors.black,
-    fontWeight: "600",
-    fontSize: 14,
-    marginTop: 8,
-  },
-});
+const styles = StyleSheet.create({});
--- Colors.js
+++
@@ -1,8 +0,0 @@
-export default colors = {
-  black: "#2D3436",
-  gray: "#A4a4a4",
-  lightGray: "#CACACA",
-  blue: "#24A6D9",
-  lightBlue: "#A7CBD9",
-  white: "#FFFFFF",
-};

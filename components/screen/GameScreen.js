fc227f99c06f95fadc49442adfce410f509fa262
Dat Manh Vo <vomanhdat10998@gmail.com>
2021-02-10 17:36:38 +0700

    add lower and higher button

--- components/screen/GameScreen.js
+++ components/screen/GameScreen.js
@@ -1,30 +1,62 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import React, { useRef, useState } from "react";
+import { View, Text, StyleSheet, Button, Alert } from "react-native";
 import Card from "../card/Card";
 import NumberContainer from "../number-container/NumberContainer";
 
-const generateNumber = (min, max, soChon) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  //   Create random number
-  const rndNumber = Math.floor(Math.random() * (max - min) + min);
-
-  if (rndNumber === soChon) {
-    return generateNumber(min, max, soChon);
-  } else {
-    return rndNumber;
-  }
-};
-
 const GameScreen = ({ soNguoiDungChon }) => {
+  const generateNumber = (min, max, soChon) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    //   Create random number
+    const rndNumber = Math.floor(Math.random() * (max - min) + min);
+
+    if (rndNumber === soChon) {
+      return generateNumber(min, max, soChon);
+    } else {
+      return rndNumber;
+    }
+  };
+
   const [soDoan, setsoDoan] = useState(generateNumber(1, 100, soNguoiDungChon));
+  const soMin = useRef(1);
+  const soMax = useRef(100);
+
+  const nextGuessHandler = (condition) => {
+    if (
+      (condition === "lower" && soDoan < soNguoiDungChon) ||
+      (condition === "higher" && soDoan > soNguoiDungChon)
+    ) {
+      Alert.alert("Xin lỗi", "Bạn chọn sai rồi!", [{ text: "OK" }]);
+    }
+
+    if (condition === "lower") {
+      soMax.current = soDoan;
+    } else {
+      soMin.current = soDoan;
+    }
+
+    const randomNumber = generateNumber(soMin.current, soMax.current, soDoan);
+
+    setsoDoan(randomNumber);
+  };
+
   return (
     <View style={styles.screen}>
       <Text>Số đoán</Text>
       <NumberContainer>{soDoan}</NumberContainer>
       <Card style={styles.buttonContainer}>
-        <Button title="Thấp hơn" onPress={() => {}} />
-        <Button title="Cao hơn" onPress={() => {}} />
+        <Button
+          title="Thấp hơn"
+          onPress={() => {
+            nextGuessHandler("lower");
+          }}
+        />
+        <Button
+          title="Cao hơn"
+          onPress={() => {
+            nextGuessHandler("higher");
+          }}
+        />
       </Card>
     </View>
   );
